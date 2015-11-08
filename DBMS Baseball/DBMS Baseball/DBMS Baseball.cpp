521d49b148d5b5ad1a8c00b58b1ebda94c67dbe9
Gperez88 <perezgabriel89@gmail.com>
2015-11-08 00:10:17 -0400

    inicializacion de repositorio

---
+++ DBMS Baseball/DBMS Baseball/DBMS Baseball.cpp
@@ -0,0 +1,15 @@
+// DBMS Baseball.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Hola" << endl;
+	cin >> a;
+    return 0;
+}
+
